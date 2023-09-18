63eee5c58b34d19f09d3eacc5db039a1448102d9
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-09-18 16:50:15 +0300

    first commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { RecorderPage } from './RecorderPage';
+
+function App() {
+  return (
+    <>
+      <RecorderPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/RecorderPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const RecorderPage = () => {
+    return (
+        <div>
+            <video autoPlay className="live-player"></video>
+
+        </div>
+    )
+}
